c49b54630754639e9c9b77d66da1d6b2d39fdb55
k-gio <69139121+k-gio@users.noreply.github.com>
2020-08-03 16:45:25 +0900

    User name added

--- ex4.java
+++ ex4.java
@@ -1,9 +1,18 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class ex4 {
 
     public static void main(String[] args) {
 
+        //名前
+        System.out.println("Who are you?");
+        Scanner scanName = new Scanner(System.in);
+        String name = scanName.next();
+        System.out.println("Hello," + name);
+        System.out.println("");
+
+        //コイントス
         int headCount = 0;
         int tailCount = 0;
 
