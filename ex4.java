cc39f5ac7c760f6a729da145487b70cf5d3d9b8e
k-gio <69139121+k-gio@users.noreply.github.com>
2020-08-03 16:29:41 +0900

    HoT: Initial revision

---
+++ ex4.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class ex4 {
+
+    public static void main(String[] args) {
+
+        int headCount = 0;
+        int tailCount = 0;
+
+        String[] coin = {"Heads", "Tails"};
+        Random r = new Random();
+        
+        System.out.println("Tossing a coin...");
+
+        for(int i = 0; i < 3; i++) {
+            String result = coin[r.nextInt(2)];
+            System.out.print("Round" + (i+1) + ":");
+            System.out.println(result);
+            if(result == "Heads") {
+                headCount += 1;
+            } else {
+                tailCount += 1;
+            }
+        }
+
+        System.out.println("Heads:" + headCount + "," + "Tails:" + tailCount);
+
+
+    }
+
+
+}
